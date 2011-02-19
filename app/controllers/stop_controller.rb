def0e7d13811bea54629dd1f3a063d5b785c4b11
Nick Sewell <nsewell@gmail.com>
2011-02-19 13:24:21 +1100

    Stops with controller and view changes

--- app/controllers/stop_controller.rb
+++ app/controllers/stop_controller.rb
@@ -9,19 +9,26 @@ end
 
 class StopController < ApplicationController
   def index
-    @stops = Stopdescriptions.all
+    @stops = StopDescription.limit(10)
   end
 
   def in_region()
     latlong = [params[:lat].to_f, params[:long].to_f]
     bounds = region(latlong)
     # NOTE Southern Hemisphere -33 lat, opposite comparator for negative
-    @stops = Stopdescriptions.where("latitude  > ?", bounds[0].lat).
+    @stops = StopDescription.where("latitude  > ?", bounds[0].lat).
                               where("longitude > ?", bounds[0].long).
                               where("latitude  < ?", bounds[1].lat).
                               where("longitude < ?", bounds[1].long).
                               limit(10)
     render :index
+  end
+
+  def show()
+    tsn = params[:id]
+    @arrivals = Stops.where("tsn = ?", tsn)
+    @stop = StopDescription.where("tsn = ?", tsn).first
+    render :show
   end
 
   private
