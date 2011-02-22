93186d4690b961f462d014020eb44cc67990f212
Rufus Post <rufuspost@gmail.com>
2011-02-22 23:33:37 +1100

    added deploy script

--- Gemfile
+++ Gemfile
@@ -8,6 +8,7 @@ gem 'nokogiri'
 gem 'fastercsv'
 
 group :development, :test do
-  #gem 'rspec-rails'
-  #gem 'machinist', '2.0.0.beta2'
+  gem 'capistrano'
+  gem 'rspec-rails'
+  gem 'machinist', '2.0.0.beta2'
 end
---
+++ config/deploy.rb
@@ -0,0 +1,36 @@
+require "bundler/capistrano"
+
+set :application, "nswbus"
+set :repository,  "git@github.com:mynameisrufus/nswbus.git"
+set :scm, :git
+set :branch, "master"
+
+set :use_sudo, false
+set :user, "rails"
+set :runner, user
+set :domain, "203.25.171.42"
+set :deploy_via, "remote_cache"
+set :deploy_to, "~/www/#{application}"
+set :keep_releases, 2
+
+ssh_options[:forward_agent] = true
+ssh_options[:paranoid] = false
+ssh_options[:user] = 'rails'
+
+role :web, domain
+role :app, domain
+role :db,  domain, :primary => true
+
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
+
+task :update_config, :roles => [:app] do
+  run "cp -Rf #{shared_path}/config/* #{release_path}/config/"
+end
+
+after "deploy:update_code", :update_config
